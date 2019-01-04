8928b42d9694590f69847acb317106388b3fc347
PHONGNGUYEN\Nguyenvan <ngvanphong2012@gamil.com>
2019-01-04 17:46:21 +0700

    190401v1

---
+++ TeduCoreApp/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TeduCoreApp.Application.Interfaces;
+using TeduCoreApp.Data.ViewModels.Contact;
+
+namespace TeduCoreApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        private IContactService _contactService;
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+        [Route("contact.html")]
+        public IActionResult Index()
+        {
+            ContactViewModel contactVm = _contactService.GetContact();
+            return View(contactVm);
+        }
+    }
+}
